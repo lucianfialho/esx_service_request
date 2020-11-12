c6317b3e5aa71e1fb84767a5fd0ececdac0d3e54
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-12 08:31:01 -0300

    - add job config

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config  = {}
+Config.Jobs = {
+    ['ambulance'] = {
+        ['messageRequest'] = 'messageRequest: ambulance',
+        ['playerCallbackMessage'] = 'playerCallbackMessage: ambulance',
+        ['needSendGpsLocation'] = true
+        
+    },
+    ['cardealer'] = {
+        ['messageRequest'] = 'messageRequest: cardealer',
+        ['playerCallbackMessage'] = 'playerCallbackMessage: cardealer',
+        ['needSendGpsLocation'] = false
+    },
+    ['cardealer'] = {
+        ['messageRequest'] = 'messageRequest: cardealer',
+        ['playerCallbackMessage'] = 'playerCallbackMessage: cardealer',
+        ['needSendGpsLocation'] = false
+    }
+}
