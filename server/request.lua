d1a0063f665443646a1af5e53d02388084267dff
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-14 16:37:10 -0300

    -- add request config

--- server/request.lua
+++ server/request.lua
@@ -1,16 +1,33 @@
 local Class = module(GetCurrentResourceName(), 'utils/class')
+local uuid = module(GetCurrentResourceName(), 'utils/uuid')
 
-local Request = Class.new(function(self, xPlayersInService, job)
+local Request = Class.new(function(self, xPlayersInService, job, requester)
     self.playersInService = xPlayersInService
-    self.message =  job
+    
+    self.job =  job 
+    
+    self.request = {
+        ['id'] = uuid(),
+        ['job'] = job.name,
+        ['requester'] = requester,
+        ['receiver'] = false
+    }
 end)
 
+function Request:sendRequest(request)
+    local requestConfig = {
+        ['title'] = self.job.jobConfig.title,
+        ['message'] = self.job.jobConfig.messageRequest,
+        ['icon'] = self.job.jobConfig.icon,
+        ['requester'] = self.receiver
+    }
 
-function Request:sendRequest()
+    request['config'] = requestConfig
+    print(json.encode(#self.playersInService, {indent = true}))
+
     for k, xPlayer in ipairs(self.playersInService) do
-        xPlayer.triggerEvent('esx_request_service:SendRequesToPlayerInService')
+        xPlayer.triggerEvent('esx_request_service:SendRequesToPlayerInService', request)
     end
 end
 
 return Request
-
