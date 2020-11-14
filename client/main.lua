303113e3ae0c83442272ea7199ce8de3ed50c4c5
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-14 16:27:45 -0300

    -- add event to accept service

--- client/main.lua
+++ client/main.lua
@@ -7,10 +7,50 @@ Citizen.CreateThread(function()
 	end
 end)
 
+
 RegisterNetEvent('esx_request_service:SendRequesToPlayerInService')
-AddEventHandler('esx_request_service:SendRequesToPlayerInService', function()
-    local PlayerPed = PlayerPedId()
+AddEventHandler('esx_request_service:SendRequesToPlayerInService', function(request, requestConfig)
+	local PlayerPed = PlayerPedId()
+	
+    SendNUIMessage({
+		type = 'request',
+		request = request,
+		requestConfig = requestConfig
+	})
+	
+	local disableKeyMapping = false
+	
+	Citizen.CreateThread( function()
+		while not disableKeyMapping do
+			Citizen.Wait(0)
+			-- checks if "Y" has just been released
+			if IsControlJustReleased(0--[[input group]],  246--[[control index]]) then
+				ESX.TriggerServerCallback('esx_request_service:acceptService', function(status)
+					if not status.success then ESX.ShowNotification('Demorou demais otário!')  end
+					
+					disableKeyMapping = true
 
-    ESX.ShowNotification("asd")
+					SendNUIMessage({
+						type = 'remove',
+						request = request
+					})
+				end, request.id)
+			end
+		end
+		
+	end)
+end)
 
+
+RegisterNetEvent('esx_request_service:SendCallbackToRequester')
+AddEventHandler('esx_request_service:SendCallbackToRequester', function()
+	
+    ESX.ShowNotification("Seu pedido foi atendido com sucesso, aguarde alguns momentos")
+	
+end)
+
+
+RegisterNetEvent('esx_request_service:SetService')
+AddEventHandler('esx_request_service:SetService', function(position)
+    SetNewWaypoint(position.x --[[ number ]], position.y --[[ number ]])
 end)
